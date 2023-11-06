4440db8746f8a50a1ab880005d615967a41af9c8
Wouter Scherphof <wouter.scherphof@gmail.com>
2023-11-06 15:47:34 +0100

    dg bump (obsoleted bind.sh)

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ VOLUME=$VOLUME
 
 docker container run --restart "$RESTART" --name "$CONTAINER" \
 	-e DOCKER_ENV="$DOCKER_ENV" \
-	-v "$(docker4gis/bind.sh "$FILEPORT" /fileport)" \
+	--mount type=bind,source="$FILEPORT",target=/fileport \
 	--network "$NETWORK" \
 	-d "$IMAGE"
