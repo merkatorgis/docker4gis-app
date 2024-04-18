23ccf9f83b96f8700a214cbcb6a99eef5465b739
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-04-18 17:16:05 +0200

    wip upstream changes

--- run.sh
+++ run.sh
@@ -9,6 +9,10 @@ NETWORK=$NETWORK
 FILEPORT=$FILEPORT
 VOLUME=$VOLUME
 
+# Use fileport/$DOCKER_USER instead of fileport/$DOCKER_USER/$repo.
+FILEPORT=$FILEPORT/..
+mkdir -p "$FILEPORT"
+
 docker container run --restart "$RESTART" --name "$CONTAINER" \
 	-e DOCKER_ENV="$DOCKER_ENV" \
 	--mount type=bind,source="$FILEPORT",target=/fileport \
